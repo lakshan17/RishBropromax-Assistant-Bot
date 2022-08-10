2f7373e6868d8f2729f6cda77a39f03cc45a2a97
Rishmika Sandanu <104628935+RishBropromax@users.noreply.github.com>
2022-08-10 15:12:23 +0530

    Create ban_talk.py

---
+++ Assistant/Cmd/ban_talk.py
@@ -0,0 +1,13 @@
+from Assistant import app
+from Assistant.Cmd.commands import *
+
+@app.on_message(command("ban"))
+async def plug(_, message):
+    RishBroProMax = await message.reply_text(text="he he he he Good bye !! See You Again"
+    )
+    RishBroproMax = """
+I m Assistant Bot Of Rishmika Sandanu
+@ImRishmika_Bot   
+    """
+    await RishBroproMax.edit_text(Rishmika)
+
